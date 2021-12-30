c761919a50caaa8ba612cb2e6cedfc9cf08c66e9
Chirag Seth <chiragseth30sep@gmail.com>
2021-12-30 18:05:59 +0530

    add

--- Fibonacci/Main.java
+++ Fibonacci/Main.java
@@ -5,12 +5,13 @@ public class Main{
   public static void main(String[] args) {
       Scanner scn = new Scanner(System.in);
 
-      int n = Scn.nextInt();
+      int n = scn.nextInt();
       int a = 0;
       int b = 1;
 
       for(int i = 1; i<= n; i++){
-          System.out.print(a);
+          System.out.println
+          (a);
           int c = a + b;
           a = b;
           b = c;
