4820db8dabb539d9b937b4ff15e526a34847de50
Chirag Seth <91310725+chirag300@users.noreply.github.com>
2021-12-30 18:03:14 +0530

    Update Main.java

--- Fibonacci/Main.java
+++ Fibonacci/Main.java
@@ -5,7 +5,7 @@ public class Main{
   public static void main(String[] args) {
       Scanner scn = new Scanner(System.in);
 
-      int n = Scn.nextInt();
+      int n = scn.nextInt();
       int a = 0;
       int b = 1;
 
