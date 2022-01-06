dbf44d48385decb12af499a43fd25da770c70ec8
Chirag Seth <chiragseth30sep@gmail.com>
2022-01-06 12:39:03 +0530

    add

--- Reverse a number/Main.java
+++ Reverse a number/Main.java
@@ -1,3 +1,18 @@
-public class Main {
+import java.util.Scanner;
+
+public class Main{
+   
+   public static void main(String[] args) {
+     Scanner scn = new Scanner(System.in);
+     int n = scn.nextInt();
+     
+     while(n != 0){
+         
+         int lastdigit = n%10;
+         System.out.println(lastdigit);
+         
+         n = n/10;
+     }
     
-}
+    }
+   }
